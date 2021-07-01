8c958f1dd5e217003359c0ae1b9a1950a3a9b0bb
fatehalive <fatehalive@gmail.com>
2021-07-01 20:48:19 +0700

    just delete .env + another files

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
-const dotenv = require('dotenv');
-dotenv.config();
+require("dotenv").config({ path: "config/.env"});
 
 const express = require("express");
 const app = express();
@@ -8,11 +7,11 @@ const sequelize = require('./util/databaseconnect');
 const routes = require('./router/routes');
 
 // Middleware
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 // Routes
-app.use(routes)
+app.use(routes);
 
 sequelize.sync()
     .then(app.listen(5000, console.log('server is running at port 5000')))
--- server/util/databaseconnect.js
+++ server/util/databaseconnect.js
@@ -1,12 +1,9 @@
 const Sequelize = require('sequelize');
 
-// Note: bagi yang belum terhubung, bisa sesuaikan config masing2. Sequelize(<database>, <user>, <password>).
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     dialect: 'postgres',
-    host: process.env.DB_HOST || 'localhost',
-    // Opsional untuk port / jika portnya 3306 untuk mysql maka tidak perlu menulis port
-    // Nggak mau di port 5400, saya ganti default port postgresql 5432 baru bisa.
-    port: process.env.DB_PORT || 5432
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT
 });
 
 module.exports = sequelize;
