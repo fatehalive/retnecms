c7321680ee44400f4400f6c6db7642510ce4f049
aflahaayub <aflaha25@gmail.com>
2021-07-27 22:09:58 +0700

    Get data from axios into article

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,23 @@
 import * as React from 'react'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-
 // component
 import SinglePost from './pages/SinglePost/SinglePost8'
-import HomePage from './pages/Homepages/Homepage6'
 import Header from './Component/Header/MainHeader'
 import Footer from './Component/Home/Footer/Footer'
+import HomePage from './pages/Homepages/Homepage6'
 
 function App() {
-
   return (
     <div>
-      <Router>
-        {/* HEADER */}
-        <Header/>
-        {/* MAIN */}
-        <Switch>
-          <Route path="/article">
-            <SinglePost/>
-          </Route>
-          <Route path="/">
-            <HomePage/>
-          </Route>
-        </Switch>
-
-        {/* FOOTER */}
-        <Footer/>
-      </Router>
+          {/* HEADER */}
+          <Header/>
+          {/* MAIN */}
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route path="/:articleId" component={SinglePost} />
+            </Switch>
+          {/* FOOTER */}
+          <Footer/>
     </div>
   );
 }
