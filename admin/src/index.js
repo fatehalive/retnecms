a4dcef4585d8d9b942d88ef24ac81f36c20f515f
fatehalive <fatehalive@gmail.com>
2021-08-08 16:26:37 +0700

    Add Authorization Request Header

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import axios from 'axios';
 // redux
 import { Provider } from 'react-redux';
 import Store from './redux/Store.js';
@@ -9,7 +10,25 @@ import Admin from './layouts/Admin';
 import Auth from './layouts/Auth';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import AuthRoutes from './components/PrivateRoute/AuthRoutes';
+import authHeader from './services/auth-header.js';
 // import NotFound from './views/NotFound';
+
+let x = authHeader();
+axios.defaults.headers.common['Authorization'] = x.Authorization;
+
+axios.interceptors.request.use(request => {
+    // Edit request config
+    return request;
+}, error => {
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+    // Edit response config
+    return response;
+}, error => {
+    return Promise.reject(error);
+});
 
 ReactDOM.render(
     <Provider store={Store}>
