756316bca4544d6362817d2928bbff88a070aebc
fatehalive <fatehalive@gmail.com>
2021-08-12 19:44:44 +0700

    .gitignore working again

--- admin/src/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    WS_BASE_URL: 'http://localhost:5000', // process.env.API_URL || 'http://api.retnecms.com'
-    DOMAIN_NAME: 'http://localhost:4000' // process.env.DOMAIN || 'http://admin.retnecms.com'
-}
-
-export default config;
