4ac1625356081e5c254b66c1b9ed9bcf7b8f997c
didihottest <didihottest@gmail.com>
2021-08-12 22:22:54 +0700

    conditional navbar

--- admin/src/components/Sidebar/Sidebar.js
+++ admin/src/components/Sidebar/Sidebar.js
@@ -1,6 +1,23 @@
 import { NavLink } from 'react-router-dom';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux'
+import React, { useEffect, useMemo, useState } from 'react';
 
 function SideBar() {
+    const { currentState } = useSelector(
+        (state) => ({ currentState: state.whois }),
+        shallowEqual
+    )
+    const { currentUser } = currentState
+
+    const [user, setUser] = useState([])
+
+    useEffect(() => {
+        setUser(currentUser)
+    }, [currentUser])
+
+    const categoryAllow = ['superadmin', 'editor']
+    const usersAllow = ['superadmin']
+    const postsAllow = ['superadmin', 'writer', 'editor']
     return (
         <aside className="sidebar sidebar-left">
             <div className="sidebar-content">
@@ -16,32 +33,55 @@ function SideBar() {
                         <li>
                             <NavLink to="/admin/preview" target="_blank"><i className="icon dripicons-preview"></i><span className="mr-3">Preview Site</span><i className="ml-5 la la-external-link"></i></NavLink>
                         </li>
+                        {
+                            user && user.role && user.role.role && postsAllow.includes(user.role.role) && (
+                                <li>
+                                    <NavLink to="/admin/posts/index"><i className="icon dripicons-document-edit"></i><span>Posts List</span></NavLink>
+                                </li>
+                            )
+                        }
 
-                        <li className="nav-dropdown">
+                        {/* <li className="nav-dropdown">
                             <NavLink to="/admin/posts/index" className="has-arrow"><i className="icon dripicons-document-edit"></i><span>Posts</span></NavLink>
                             <ul className="collapse nav-sub">
-                                <li><NavLink to="/admin/posts/index"><span>List</span></NavLink></li>
+                                
                                 <li><NavLink to="/admin/posts/draft"><span>Draft</span></NavLink></li>
                                 <li><NavLink to="/admin/posts/published"><span>Published</span></NavLink></li>
                             </ul>
-                        </li>
+                        </li> */}
 
-                        <li>
-                            <NavLink to="/admin/categories/index"><i className="icon dripicons-tags"></i><span>Categories</span></NavLink>
-                        </li>
+                        {
+                            user && user.role && user.role.role && categoryAllow.includes(user.role.role) &&
+                            <li>
+                                <NavLink to="/admin/categories/index"><i className="icon dripicons-tags"></i><span>Categories</span></NavLink>
+                            </li>
+                        }
 
-                        <li className="nav-dropdown">
+
+                        {/* <li className="nav-dropdown">
                             <NavLink to="/admin/users/index" className="has-arrow"><i className="icon dripicons-user-group"></i><span>Users</span></NavLink>
                             <ul className="collapse nav-sub">
-                                <li><NavLink to="/admin/users/index"><span>List</span></NavLink></li>
-                                <li><NavLink to="/admin/roles/index"><span>Roles</span></NavLink></li>
+
+                                <li><NavLink to="/admin/users/index"><i className="icon dripicons-user-group"></i><span>List</span></NavLink></li>
+                                <li><NavLink to="/admin/roles/index"><i className="icon dripicons-user-group"></i><span>Roles</span></NavLink></li>
                             </ul>
-                        </li>
+
+                        </li> */}
+                        {
+                            user && user.role && user.role.role && usersAllow.includes(user.role.role) &&
+                            <>
+                                <li><NavLink to="/admin/users/index"><i className="icon dripicons-user-group"></i><span>Users List</span></NavLink></li>
+                                <li><NavLink to="/admin/roles/index"><i className="icon dripicons-network-3"></i><span>Roles List</span></NavLink></li>
+
+                            </>
+                        }
+
+
 
                     </ul>
                 </nav>
-            </div>
-        </aside>
+            </div >
+        </aside >
     )
 };
 
